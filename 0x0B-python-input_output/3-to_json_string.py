95d7a91c95c1860a93fac19e3de4d10d8303c0b0
Portia201 <mologadi80@gmail.com>
2024-02-07 15:20:19 +0000

    3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
-def append_write(filename="", text=""):
-    with open(filename, "a", encoding="UTF-8") as f:
-    
+
+import json
+
+
+def to_json_string(my_obj):
+    return json.dumps(my_obj)
