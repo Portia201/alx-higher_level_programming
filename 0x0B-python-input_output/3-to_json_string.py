a52cd3e4121f561e5817e88f852c8bf8451e7c09
Portia201 <mologadi80@gmail.com>
2024-02-19 09:36:04 +0000

    json steing

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-
+"""Defines a string-to-JSON function."""
 
 import json
 
-
 def to_json_string(my_obj):
+    """Return the JSON representation string of a objet."""
     return json.dumps(my_obj)
